1066df9f804f3f808e3e023d41a8946a53ee9f83
youssefhanin777 <122698298+youssefhanin777@users.noreply.github.com>
2024-03-14 22:56:08 +0000

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     discord: {
         id: process.env.DISCORD_ID,
         prefix: '!',
-        footer: `© Corwin 2021 - ${new Date().getFullYear()}`, 
-        botInvite: `https://discord.com/oauth2/authorize?&client_id=840212110817755157&scope=applications.commands+bot&permissions=8`,
-        serverInvite: "https://discord.gg/qHXxpQKC2H",
+        footer: `© Good People 2020 - ${new Date().getFullYear()}`, 
+        botInvite: `https://discord.com/oauth2/authorize`,
+        serverInvite: "https://discord.gg/7yerwHMFFN",
     },
 
     wordList: `Airplane
