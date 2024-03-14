aa8d5305350e1604996e4916d9aab1bc85e1b070
youssefhanin777 <122698298+youssefhanin777@users.noreply.github.com>
2024-03-14 22:52:17 +0000

    Update static.js

--- src/assets/utils/static.js
+++ src/assets/utils/static.js
@@ -2,10 +2,10 @@ module.exports.defaultPFP = 'https://cdn.discordapp.com/embed/avatars/0.png';
 
 module.exports.DummyUser = {
     bot: false,
-    id: '00000000000',
-    tag: "Unknown User#0000",
-    name: "Unknown User",
-    username: "Unknown User",
+    id: '1142476088472248320',
+    tag: "GoodPeople",
+    name: "youzarx",
+    username: "<@780906998962061343>",
     hexAccentColor: "#FFFFFF",
     avatarURL: () => 'https://cdn.discordapp.com/embed/avatars/0.png'
 }
